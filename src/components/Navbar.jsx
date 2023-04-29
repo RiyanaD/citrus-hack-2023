1b88a4d99a6761013fa41fd420d69200b80dd55f
Arkes123 <jonathan.darius2015@gmail.com>
2023-04-29 11:03:48 -0700

    Created Navbar with logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <div>hello</div>
+        <nav className="bg-black dark:bg-grey-900">
+            <div className="w-screen h-[80px] z-10 bg-white fixed drop-shadow-md">
+                <div className='px-2 flex justify-between items-center w-full h-full'>
+                    <div className="flex items-center">
+                        <h1 className="text-4xl item Center font-bold pl-5">Veritas</h1>
+                        <button className="flex justify-between mr-auto">About Our Project</button>
+                    </div>
+                </div>
+            </div>
+        </nav>
     );
 };
 
