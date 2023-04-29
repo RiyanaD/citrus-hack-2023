5458561126278ff7513b522975f9106da0c5a938
RiyanaD <riyana.duttaa@gmail.com>
2023-04-29 15:21:19 -0700

    add input file

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const Input = () => {
     return (  
@@ -6,7 +6,9 @@ const Input = () => {
             <form>
                 <div className="text-white text-center font-mono">Enter link: </div>
                 <div className = "flex justify-center">
-                    <input type="text" className="w-2/3 bg-white rounded-md border-gray-300 border px-3 py-2 placeholder-gray-400 text-gray-700 focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500 focus:ring-opacity-50" placeholder="Enter URL here" />
+                    <input 
+                        type="text" 
+                        className="w-2/3 bg-white rounded-md border-gray-300 border px-3 py-2 placeholder-gray-400 text-gray-700 focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500 focus:ring-opacity-50" placeholder="Enter URL here" />
                 </div>
             </form>
         </div>
